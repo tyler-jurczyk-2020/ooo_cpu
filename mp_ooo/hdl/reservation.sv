911f47dd96fa4d3856da7af227e04bfa231a3fe6
Soumil Gupta <soumilg2@illinois.edu>
2024-04-01 00:00:23 -0500

    Current fixes

--- mp_ooo/hdl/reservation.sv
+++ mp_ooo/hdl/reservation.sv
@@ -29,7 +29,7 @@ import rv32i_types::*;
 // through the functional unit first, then it won't be updating the same source register
 // that an older entry is depending on 
 
-reserevation_entry_t reservation_table[reservation_table_size]; 
+reserevation_entry_t reservation_table[SS][reservation_table_size]; 
 //update size based on reservation table size
 logic [2:0] counter; 
 logic table_full;
@@ -45,49 +45,44 @@ always_ff @ (posedge clk) begin
     // Enter to table
     else if(~table_full) begin
         // insert new entry if there's an available one. Maintain counter for space
+        // Additional for loop because we will have N-entries at once based on N-way superscalar
         for(int i = 0; i < SS; i++) begin 
             for(int j = 0; j < reservation_table_size; j++) begin
                 if(~reservation_table[j].valid) begin
                     reservation_table[j].reservation_entry <= reservation_entry[i]; 
                     reservation_table[j].valid <= '1; 
+                    // MUST break because otherwise the entry will be put in to every available spot in the table
                     counter <= counter + 3'd1; 
                     break; 
                 end
             end
         end
     end
-end
-
-// Check all table entries to see whether we need to update them
-always_ff @ (posedge clk) begin
+    // Check all table entries to see whether we need to update them
     for(int j = 0; j < reservation_table_size; j++) begin
         if(reservation_table[j].reservation_entry.rs1_source == updated_rob) begin
-            // reservation_table[j].reservation_entry.rs1_met <= '1; // Illegal Driver 
+            reservation_table[j].reservation_entry.rs1_met <= '1;  
         end
         if(reservation_table[j].reservation_entry.rs2_source == updated_rob) begin
-            // reservation_table[j].reservation_entry.rs2_met <= '1; // Illegal Driver
+            reservation_table[j].reservation_entry.rs2_met <= '1; 
         end
     end
-end
-
-// Check all table entries to see if we can release them 
-always_ff @ (posedge clk) begin
+    // Check all table entries to see if we can release them 
     for(int j = 0; j < reservation_table_size; j++) begin
         if(fu_enable) begin
             if(reservation_table[j].reservation_entry.rs1_met && reservation_table[j].reservation_entry.rs2_met) begin
-                inst_for_fu <= reservation_entry[0]; // Not correct, need to handle both entries in superscalar 
-                // reservation_table[j].valid <= '0; // Illegal Driver
-                // counter <= counter - 3'd1;  // Illegal Driver
+                inst_for_fu <= reservation_entry[j]; // Not correct, need to handle both entries in superscalar 
+                reservation_table[j].valid <= '0; 
+                counter <= counter - 3'd1;  
                 break; 
             end
         end
     end
 end
 
-
 always_comb begin
     station_full = '0; 
-    if({{29{1'b0}},counter} >= ROB_DEPTH-2) begin // Probably need to fix width
+    if({{29{1'b0}},counter} >= ROB_DEPTH-SS) begin // Probably need to fix width
         station_full = '1; 
     end
 end
