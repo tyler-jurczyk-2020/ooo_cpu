aa881edc6f822ca1e939c67c5c9a80136516fddc
Stanley Wu <zaizhou3@illinois.edu>
2024-04-25 15:22:19 -0500

    patch 5

--- mp_ooo/synth/get_clock.py
+++ mp_ooo/synth/get_clock.py
@@ -1,4 +1,5 @@
 import json
+import sys
 import os
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
@@ -8,7 +9,7 @@ with open("options.json") as f:
     j = json.load(f)
 
 if "clock" not in j:
-    print('key "clock" not in options.json')
+    print('key "clock" not in options.json', file=sys.stderr)
     exit(1)
 
 print(int(j["clock"]))
